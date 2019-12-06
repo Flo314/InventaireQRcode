f827bba11134818cdcd50a919413d25a5a4fad88
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-06 11:53:02 +0100

    Mise en place de la navigation dans l'Activity

--- app/src/main/java/com/example/inventaireqrcode/MainActivity.kt
+++ app/src/main/java/com/example/inventaireqrcode/MainActivity.kt
@@ -2,11 +2,24 @@ package com.example.inventaireqrcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    // va contenir toute la logique de navigation dans le graph
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
