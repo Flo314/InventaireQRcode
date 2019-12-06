3d87e92dbef17b7a2516fc7c225a5c6ee990b68a
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-06 09:38:21 +0100

    Class Métier qui vont permettre de manipuler les données de l'app

--- app/src/main/java/com/example/inventaireqrcode/GadgetsModels.kt
+++ app/src/main/java/com/example/inventaireqrcode/GadgetsModels.kt
@@ -1,2 +1,19 @@
 package com.example.inventaireqrcode
 
+import java.util.*
+
+/**
+ * Classe Métier
+ */
+sealed class Gadget {
+    abstract var id : Int
+    abstract var dateCreated: Date
+}
+
+data class GadgetQRCode(override var id: Int = 0,
+                        override var dateCreated: Date = Date(),
+                        val url: String): Gadget()
+
+data class GadgetNFC(override var id: Int = 0,
+                        override var dateCreated: Date = Date(),
+                        val url: String): Gadget()
