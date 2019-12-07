d27e5c8cffe81f6902c0cae26936d710dc22bbbd
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-07 12:09:25 +0100

    permission camera

--- app/src/main/java/com/example/inventaireqrcode/qrcode/QRCodeScanFragment.kt
+++ app/src/main/java/com/example/inventaireqrcode/qrcode/QRCodeScanFragment.kt
@@ -1,13 +1,20 @@
 package com.example.inventaireqrcode.qrcode
 
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 
 import com.example.inventaireqrcode.R
+
+
+private const val PERMISSION_REQUEST_CAMERA = 1
 
 /**
  * A simple [Fragment] subclass.
@@ -22,5 +29,34 @@ class QRCodeScanFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_qrcode_scan, container, false)
     }
 
+    override fun onResume() {
+        super.onResume()
+        // est qu'on a la permission
+        if (!hasCameraPermission()) {
+            requestCameraPermission()
+        }
+    }
+
+    private fun hasCameraPermission() =
+        ContextCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun requestCameraPermission() {
+        requestPermissions(arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CAMERA -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                findNavController().popBackStack()
+            }
+        }
+    }
 
 }
