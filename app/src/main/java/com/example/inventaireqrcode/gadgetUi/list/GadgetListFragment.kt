b10092409ce8b3b0829f91f2741b3561c1bc6f72
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-07 11:09:03 +0100

    mise à jour de l'ui à partir de viewstate

--- app/src/main/java/com/example/inventaireqrcode/gadgetUi/list/GadgetListFragment.kt
+++ app/src/main/java/com/example/inventaireqrcode/gadgetUi/list/GadgetListFragment.kt
@@ -8,17 +8,22 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.inventaireqrcode.App
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.inventaireqrcode.*
 
-import com.example.inventaireqrcode.R
 import com.example.inventaireqrcode.gadgetUi.GadgetUiViewModelFactory
+import kotlinx.android.synthetic.main.fragment_gadget_list.*
+import timber.log.Timber
 
 /**
  * A simple [Fragment] subclass.
  */
-class GadgetListFragment : Fragment() {
+class GadgetListFragment : Fragment(), GadgetListAdapter.GadgetListAdapterListener {
 
     private lateinit var viewModel: GadgetListViewModel
+
+    private lateinit var gadgetListAdapter: GadgetListAdapter
+    private val gadgets = mutableListOf<Gadget>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,15 +36,34 @@ class GadgetListFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        gadgetListAdapter = GadgetListAdapter(context!!, gadgets, this)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = gadgetListAdapter
+
         // exposer le viewModel sur le fragment
         val factory =
             GadgetUiViewModelFactory(App.repo)
         viewModel = ViewModelProviders.of(activity!!, factory).get(GadgetListViewModel::class.java)
         viewModel.getViewState().observe(this, Observer { updateUi(it!!) })
+
+        // test d'ajout de gadget dans la liste
+        viewModel.addGadget(GadgetQRCode(url = "https://qrcode"))
+        viewModel.addGadget(GadgetNfc(url = "https://nfc"))
     }
 
+    // mise à jour quand on reçoit un état du viewstate
     private fun updateUi(state: GadgetListViewState) {
+        Timber.i("New state=$state")
+        // on met à jour l'adapter que si on a reçu des nouveaux gadgets
+        if (state.hasGadgetChange){
+            gadgets.clear()
+            gadgets.addAll(state.gadgets)
+            gadgetListAdapter.notifyDataSetChanged()
+        }
+    }
 
+    override fun onGadgetClicked(gadget: Gadget) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 
