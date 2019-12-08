ed46e09fe6a0513de018636852dde62ddad6540f
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-08 11:18:00 +0100

    ui de l'activity sacnnfc

--- app/src/main/java/com/example/inventaireqrcode/nfc/NfcScanActivity.kt
+++ app/src/main/java/com/example/inventaireqrcode/nfc/NfcScanActivity.kt
@@ -2,7 +2,9 @@ package com.example.inventaireqrcode.nfc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import com.example.inventaireqrcode.R
+import kotlinx.android.synthetic.main.activity_nfc_scan.*
 
 class NfcScanActivity : AppCompatActivity() {
 
@@ -16,5 +18,8 @@ class NfcScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nfc_scan)
+
+        // animation sur l'image du tag nfc
+        nfcImageView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.blink))
     }
 }
