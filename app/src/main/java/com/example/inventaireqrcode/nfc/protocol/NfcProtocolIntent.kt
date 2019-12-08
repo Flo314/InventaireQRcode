09e04dd491d6681761fa5fa7b5ca88e0679c9410
florent losada <florentlosada@MacBook-Pro-de-florent.local>
2019-12-08 16:32:17 +0100

    verouiller le nfc

--- app/src/main/java/com/example/inventaireqrcode/nfc/protocol/NfcProtocolIntent.kt
+++ app/src/main/java/com/example/inventaireqrcode/nfc/protocol/NfcProtocolIntent.kt
@@ -1,8 +1,11 @@
 package com.example.inventaireqrcode.nfc.protocol
 
 import android.app.Activity
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
 import android.nfc.NfcAdapter
+import timber.log.Timber
 
 class NfcProtocolIntent(
     override val listener: NfcProtocol.NfcReaderListener?,
@@ -19,8 +22,13 @@ class NfcProtocolIntent(
 
     private var state = State.IDLE
     private var nfcAdapter: NfcAdapter? = null
+    private var pendingIntent: PendingIntent? = null
 
     private fun init() : Boolean {
+        if (state != State.IDLE) {
+            Timber.w("Invalid state for init. state=$state")
+            return false
+        }
         if (nfcAdapter == null || !nfcAdapter!!.isEnabled) {
             nfcAdapter = NfcAdapter.getDefaultAdapter(activity)
 // dès qu'on récupère le nfcadapter on doit revérifier si il est null
@@ -30,11 +38,32 @@ class NfcProtocolIntent(
             }
         }
 
+        pendingIntent = PendingIntent.getActivity(activity, 0,
+            Intent(activity, activity.javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0)
+
+        switchToState(State.INIT)
         return true
     }
 
+    private fun switchToState(state: State) {
+        // voir tous les changements d'états
+        Timber.d("Switching from ${this.state} to $state")
+        this.state = state
+    }
+
     override fun lockNfc() {
-        init()
+        if (state == State.IDLE) {
+            if (!init()) {
+                init()
+            }
+        }
+        val intentFilters = arrayOf(
+            IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED),
+            IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED),
+            IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+        )
+        nfcAdapter?.enableForegroundDispatch(activity, pendingIntent, intentFilters, null)
+        switchToState(State.LOCKED)
     }
 
     override fun unlockNfc() {
